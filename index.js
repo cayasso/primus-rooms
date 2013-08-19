7c81ee00c48ac7888ecf9abda8ec8d7d0046a922
Jonathan Brumley <cayasso@gmail.com>
2013-08-19 17:16:15 -0600

    small refacoring and clean up

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./lib');
+/**
+ * Module dependencies.
+ */
+
+var PrimusRooms = require('./lib');
+
+/**
+ * Exports modules.
+ */
+
+exports.server = function server (primus, options) {
+  primus.$ = primus.$ || {};
+  primus.$.PrimusRooms = PrimusRooms;
+  PrimusRooms(primus, options);
+};
+
+exports.Adapter = PrimusRooms.Adapter;
+exports.Rooms = PrimusRooms.Rooms;
+exports.PrimusRooms = PrimusRooms;
